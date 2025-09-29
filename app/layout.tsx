a4ffcc058979ea5c4a0fce2d8abc651c1f69bc8d
Bruno Giugno <brunogiugno@gmail.com>
2025-09-29 15:55:33 -0300

    icono agregado

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,33 @@ const outfit = Outfit({
 });
 
 export const metadata: Metadata = {
-  title: "Admin Bruno Giugno",
+  title: "BrunoCars",
   description: "Rentals cars by Bruno Giugno",
+
+  icons:{
+    icon:[
+      {
+        // 🔑 Ícono principal: Usamos una alta resolución (192x192) y cambiamos el parámetro v=4
+        url:"https://ik.imagekit.io/fefgntjox/Rents-Cars/untitled-0.png?updatedAt=1758552652106&v=4", 
+        sizes:"192x192", 
+        type:"image/png"
+      },
+      // 🔑 Resolución máxima (para pantallas HiDPI/Retina)
+      {
+        url:"https://ik.imagekit.io/fefgntjox/Rents-Cars/untitled-0.png?updatedAt=1758552652106&v=4", 
+        sizes:"512x512", 
+        type:"image/png"
+      },
+    ],
+    // También es buena práctica configurarlo en 'apple'
+    apple: [
+      {
+        url:"https://ik.imagekit.io/fefgntjox/Rents-Cars/untitled-0.png?updatedAt=1758552652106&v=4",
+        sizes: "180x180",
+      },
+    ],
+    shortcut:[],
+  }
 };
 
 export default function RootLayout({
